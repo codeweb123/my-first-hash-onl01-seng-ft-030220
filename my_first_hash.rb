c7c2e84fc56b331bee45e7b1757d2b9aac8a45d2
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-06 20:42:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@ def my_hash
 end
 
 def shipping_manifest
-  return the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3}
 end
 
 def retrieval
@@ -12,7 +12,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-return shipping_manifest["oil paintings"]
+end
+shipping_manifest["oil paintings"]
 end
 
 def adding
